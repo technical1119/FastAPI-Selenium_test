eee07fc0c7d52aa7d35a54a90d18e4e53d5be8f5
technical1119 <o.galych@prolife.de>
2024-12-28 15:57:03 +0100

    finall update

--- extract.py
+++ extract.py
@@ -7,13 +7,7 @@ def createDriver() -> webdriver.Chrome:
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    
-    prefs = {"profile.managed_default_content_settings.images":2}
-    chrome_options.headless = True
-
-
-    chrome_options.add_experimental_option("prefs", prefs)
-    myDriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
     return myDriver
 
